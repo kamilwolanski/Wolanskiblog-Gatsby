1f2bbdd5eaef73b1d0583252f2efc46d02cd8b07
Kamil Wolański <kwolanski3@gmail.com>
2022-01-23 22:49:42 +0100

    added gatsby-ssr with script google optimize

---
+++ gatsby-ssr.js
@@ -0,0 +1,11 @@
+
+const React = require('react')
+
+exports.onRenderBody = ({ setHeadComponents }) => {
+  if (process.env.NODE_ENV === `production`) {
+    // ADDS GOOGLE OPTIMIZE CODE
+    setHeadComponents([
+        <script src="https://www.googleoptimize.com/optimize.js?id=OPT-W5XTX9R"></script>
+    ])
+  }
+}
