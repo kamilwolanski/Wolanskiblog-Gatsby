a8fc68af525f6e48fecbb63847651f595e63a358
Kamil Wolański <kwolanski3@gmail.com>
2022-01-24 10:16:46 +0100

    asdcxcv

--- plugins/gatsby-plugin-anti-flicker/gatsby-ssr.js
+++ plugins/gatsby-plugin-anti-flicker/gatsby-ssr.js
@@ -10,7 +10,7 @@ const optimizeAntiFlickerScript = (containerId) => ({
 h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
 (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
 })(window,document.documentElement,'async-hide','dataLayer',4000,
-{OPT-W5XTX9R:true});`,
+{'OPT-W5XTX9R':true});`,
 });
 const customHeadComponents = [
   <script dangerouslySetInnerHTML={createDataLayer()} />,
